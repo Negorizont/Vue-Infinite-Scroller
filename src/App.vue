57780fa8fbb10711a3922302eb58ab9b89c15030
Negorizont <boltuev01@mail.ru>
2020-03-09 11:35:51 +0400

    feat: create infinite scroller

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Posts/>
+  </div>
+</template>
+
+<script>
+import Posts from './components/Posts'
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Posts
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.wrapper {
+  max-width: 1020px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="header">
+    <div class="wrapper">
+      <nav class="navbar">
+        <a class="logo" href="#">posts</a>
+        <button class="button_menu">menu</button>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  .header {
+    background-color: #F8FF6E;
+  }
+
+  .navbar {
+    padding: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .logo {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 20px;
+    color: #191974;
+    text-decoration: none;
+    transition: .3s;
+  }
+
+  .logo:hover {
+    opacity: .7;
+  }
+
+  .button_menu {
+    background: none;
+    padding: 3px 5px;
+    border: 2px solid #191974;
+    border-radius: 5px; 
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 20px;
+    color: #191974;
+    transition: .3s;
+  }
+
+  .button_menu:hover {
+    background-color: #FFDD00;
+    cursor: pointer;
+  }
+</style>
